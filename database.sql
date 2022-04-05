baf96dae933a1eae1e6b3cf8b86b7b71b6f7a59b
mike corcoran <corcoran909@gmail.com>
2022-04-05 21:02:41 +0100

    altered tables

--- database.sql
+++ database.sql
@@ -18,11 +18,13 @@ CREATE TABLE IF NOT EXISTS quantity (
     id INTEGER NOT NULL AUTO_INCREMENT,
     recipe_id INTEGER NOT NULL,
     ingredient_id INTEGER NOT NULL,
+    ingredient_prep_id INTEGER NOT,
     amount INTEGER NOT NULL,
     unit VARCHAR(10) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (recipe_id) REFERENCES recipe(id),
-    FOREIGN KEY (ingredients_id) REFERENCES ingredients(id) 
+    FOREIGN KEY (ingredients_id) REFERENCES ingredients(id)
+    FOREIGN KEY (ingredient_prep_id) REFERENCES ingredient_prep(id), 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS method (
@@ -36,12 +38,8 @@ CREATE TABLE IF NOT EXISTS method (
 
 CREATE TABLE IF NOT EXISTS ingredient_prep (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    recipe_id INTEGER NOT NULL,
-    ingredient_id INTEGER NOT NULL,
     prep_description VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
-    FOREIGN KEY (recipe_id) REFERENCES recipe(id),
-    FOREIGN KEY (ingredients_id) REFERENCES ingredients(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS cuisine (
@@ -50,5 +48,9 @@ CREATE TABLE IF NOT EXISTS cuisine (
     PRIMARY KEY (id),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `recipes` VALUES (1, 'Pea and Cauliflower Curry', 'Crush the chillies and ginger together with a pinch of salt to make fine masala paste. /n Heat the oil, cumin and mustard seeds. When the mustard seeds begin to pop reduce heat to low and add the asafetida /n Add the cauliflower , return heat to medium and stir in the masala paste, turmeric, ground coriander, salt and sugar. /n Cover the pan and cook for 8-10 minutes, stirring every few minutes. /n Stir in the peas and the and tomato, cover the pan and cook fro another for a further 3-5 minutes. /n Remove from the heat and sprinkle with chopped coriander. /n Leave to rest for at least 5 minutes.', 'Curry');
-
+INSERT INTO `recipes` VALUES (1, 'Pea and Cauliflower Curry',1);
+INSERT INTO 'ingredients' VALUES (1, 'sunflower oil'), (2, 'cuin seeds'), (3, 'bown mustard seeds'), (4, 'asafetida'), (5, 'cauliflower'), (6, 'turmeric'), (7, 'ground coriander'), (8, 'salt'), (9, 'sugar'), (10, 'frozen peas'), (11, 'medium tomato'), (12, 'fresh coriander') (13, 'fresh green chillies'), (14, 'root ginger');
+INSERT INTO 'method' VALUES (1, 1, 1, 'Crush the chillies and ginger together with a pinch of salt to make a fine masala paste'), (2, 1, 2, 'Heat the oil in non-stick pan over a medium heat for 30s, then add the cumin and mustard seeds. When the mustard seeds start to pop reduce the heat and stir in teh asafetida '), (3, 1, 3, 'Add the cauliflower and return the heat to medium. Stir in the masala paste, turmeric, ground coriander, salt, and sugar. Cover the pan and leave to cook for 8-10 minutes, stirring every few minutes.'), (4,1,4, 'stir in the peas and tomato and cook for a further 3-5 minutes'), (5,1,5, 'Remove from the heat and sprinkle with the chopped coriander. Leave for 5 minutes to let flavours develop');
+INSERT INTO 'ingredient_prep' VALUES (1, 'chopped'), (2, 'finely chopped'), (3, 'roughly chopped');
+INSERT INTO 'cuisine' VALUES (1, 'curry'), (2, 'Salad');
+INSERT INTO 'quantity' VALUES () 
