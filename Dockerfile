9548868ffe24fa8b671357888b2c087527b40850
mike corcoran <corcoran909@gmail.com>
2022-04-09 23:09:55 +0100

    still no working db

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.10
 copy . .
-RUN pip install -r requirements.txt
+RUN pip3 install -r requirements.txt
 ENTRYPOINT ["python3", "app.py"]
--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import pymysql
 
 app = Flask(__name__)
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:recipe@db/appdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:recipe@db/appdb'
 app.config['SECRET_KEY'] = 'sdfgoikh'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
