ad4b6ae1ceecb907f5a47927ba8448184ac7b15e
cleon909 <corcoran909@gmail.com>
2022-04-10 10:22:20 +0100

    working docker images and volume

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:recipe@db/appdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:recipes@db/appdb'
 app.config['SECRET_KEY'] = 'sdfgoikh'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
