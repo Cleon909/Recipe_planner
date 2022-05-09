7a0e4a56132ee193414bd95f290f7dd5bc4b19dc
mike corcoran <corcoran909@gmail.com>
2022-05-09 19:47:18 +0100

    refactored some code

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:recipe@db/appdb'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:recipe@db/appdb'
 app.config['SECRET_KEY'] = 'sdfgoikh'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
